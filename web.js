63e6063c4f0a29e3938818d562a7a0e32a18986b
Maurice ADJANOR <mauriceadjanor@yahoo.com>
2013-07-14 10:26:41 +0000

    greeting from text

--- web.js
+++ web.js
@@ -2,8 +2,20 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+ readIndexData = function(){
+	var txt="";
+	fs.readFile('index.txt', function (err, data) {
+  		if (err) throw err;
+  		//console.log(data);
+		txt=data.toString('utf8');
+	});
+	return txt;
+};
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+	resptxt=readIndexData();
+  	response.send(resptxt);
 });
 
 var port = process.env.PORT || 5000;
